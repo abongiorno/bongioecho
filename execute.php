3be279103facb42bb34d50cd6b1dc0f57c74443d
Bongio <andrea.bongiorno@gmail.com>
2018-12-14 17:40:14 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -21,7 +21,7 @@ $text = strtolower($text);
 
 header("Content-Type: application/json");
 
-$date = $date->format('Y-m-d H:i:s')
+$date = $date->format('Y-m-d H:i:s');
 
 $parameters = array('chat_id' => $chatId, "text" => $username . " " . $text . " " . $date );
 $parameters["method"] = "sendMessage";
