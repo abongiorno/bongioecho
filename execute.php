cb56efcb15d225dd35bf33877643c6698b1c2550
abongiorno <andrea.bongiorno@gmail.com>
2018-12-14 15:02:33 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -20,6 +20,6 @@ $text = trim($text);
 $text = strtolower($text);
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $username +" "+$text);
+$parameters = array('chat_id' => $chatId, "text" => $username . $text);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
