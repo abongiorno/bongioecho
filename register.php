da41c538b5c483f28a228115ec6b282ce12dee00
abongiorno <abongiorno@alteanet.it>
2018-12-14 14:44:54 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://bongioecho.herokuapp.com/execute.php';
+$BOT_TOKEN = '737329338:AAGUl1zeg8_ugzi4IALFAyIzuXvAqif9HGg';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
